5426580e31bbb6eb4fbd9d56d6d58d5df6c43e3a
Owen Nelson <onelson@gmail.com>
2021-09-04 16:05:31 -0700

    Add game modes.

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -1,17 +1,67 @@
 use bracket_lib::prelude::*;
 
-struct State;
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "P: Play Game");
+        ctx.print_centered(9, "Q: Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+    fn dead(&mut self, ctx: &mut BTerm) {
+        todo!()
+    }
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal!");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::End => self.dead(ctx),
+        }
     }
+}
+
+enum GameMode {
+    Menu,
+    Playing,
+    End,
 }
 
 fn main() -> BResult<()> {
     let ctx = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
-    main_loop(ctx, State {})
+    main_loop(ctx, State::new())
 }
