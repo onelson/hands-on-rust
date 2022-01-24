6e449d6df488ebd08c0f68244dfee3c7832e518b
Owen Nelson <onelson@gmail.com>
2022-01-23 22:34:32 -0800

    Add camera module.

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -1,13 +1,14 @@
+mod camera;
 mod map;
 mod map_builder;
 mod player;
-
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub use crate::camera::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
@@ -18,6 +19,7 @@ use prelude::*;
 struct State {
     map: Map,
     player: Player,
+    camera: Camera,
 }
 
 impl State {
@@ -28,6 +30,7 @@ impl State {
         Self {
             map: map_builder.map,
             player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start),
         }
     }
 }
